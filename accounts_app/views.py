7884ce55c8d8a52b5ac592a6c6f5879427ee77b1
Григорій . <mcwertyge@gmail.com>
2025-02-14 21:53:53 +0200

    redirect fix

--- accounts_app/views.py
+++ accounts_app/views.py
@@ -20,9 +20,9 @@ class UserRegisterView(RedirectAuthenticatedUserMixin, View):
         if form.is_valid():
             user = form.save()
             login(request, user)
-            return redirect('login')
+            return redirect("login")
         return render(request, 'accounts_app/register.html', {'form': form})
-
+    
 class UserLoginView(RedirectAuthenticatedUserMixin, LoginView):
     template_name = 'accounts_app/login.html'
     redirect_authenticated_user = True
